e33879d574da8743169201fab8fd10261b5c98a4
kennethyu <uyhtennek@outlook.com>
2024-06-07 15:04:42 +0800

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Input box has a "add" button
+// Tasks can set priority (high, medium, low)
+// Task box has a priority tag, and a "check" button
+//    once the "check" button is clicked, the task box is deleted
+// Responsive
+
+
+// Fly in, fly out animation
+// Categorized
+// Re-order
+
+const taskForm = document.querySelector('form');
+
+taskForm.addEventListener('submit', evt => {
+
+});
